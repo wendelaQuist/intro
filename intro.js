d8c8ab1cbb075e97147458d2ea3f2c07cacb8b06
wendelaQuist <wendela140200@gmail.com>
2022-02-22 14:02:58 +0100

    getting up

--- intro.js
+++ intro.js
@@ -1,9 +1,15 @@
 setTimeout(function(){
-    // Your function here e.i
     document.getElementById('second-shot').style.display = 'none';
 }, 12000)
 
 setTimeout(function(){
-    // Your function here e.i
     document.getElementById('third-shot').style.display = 'flex';
 }, 12010)
+
+setTimeout(function(){
+    document.getElementById('third-shot').style.display = 'none';
+}, 14000)
+
+setTimeout(function(){
+    document.getElementById('fourth-shot').style.display = 'flex';
+}, 14010)
