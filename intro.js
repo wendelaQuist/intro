7886ddd5cb2e321c2941dffb190a17655534d6b3
wendelaQuist <wendela140200@gmail.com>
2022-02-22 23:01:13 +0100

    chicks swim

--- intro.js
+++ intro.js
@@ -23,3 +23,43 @@ setTimeout(function(){
 setTimeout(function(){
     document.getElementById('fifth-shot').style.display = 'flex';
 }, 18000)
+
+setTimeout(function(){
+    document.getElementById('fifth-shot').style.display = 'none';
+}, 29000)
+
+setTimeout(function(){
+    document.getElementById('sixth-shot').style.display = 'flex';
+}, 29000)
+
+setTimeout(function(){
+    document.getElementById('sixth-shot').style.display = 'none';
+}, 32000)
+
+setTimeout(function(){
+    document.getElementById('seventh-shot').style.display = 'flex';
+}, 32000)
+
+setTimeout(function(){
+    document.getElementById('seventh-shot').style.display = 'none';
+}, 34000)
+
+setTimeout(function(){
+    document.getElementById('eight-shot').style.display = 'flex';
+}, 34000)
+
+setTimeout(function(){
+    document.getElementById('eight-shot').style.display = 'none';
+}, 36000)
+
+setTimeout(function(){
+    document.getElementById('ninth-shot').style.display = 'flex';
+}, 36000)
+
+setTimeout(function(){
+    document.getElementById('ninth-shot').style.display = 'none';
+}, 39000)
+
+setTimeout(function(){
+    document.getElementById('tenth-shot').style.display = 'flex';
+}, 39000)
