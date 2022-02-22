55c7f26c11200dfd6b4e620bce0674a5ea2bc179
wendelaQuist <wendela140200@gmail.com>
2022-02-22 14:54:20 +0100

    living room

--- intro.js
+++ intro.js
@@ -1,3 +1,5 @@
+
+
 setTimeout(function(){
     document.getElementById('second-shot').style.display = 'none';
 }, 12000)
@@ -13,3 +15,11 @@ setTimeout(function(){
 setTimeout(function(){
     document.getElementById('fourth-shot').style.display = 'flex';
 }, 14010)
+
+setTimeout(function(){
+    document.getElementById('fourth-shot').style.display = 'none';
+}, 16000)
+
+setTimeout(function(){
+    document.getElementById('fifth-shot').style.display = 'flex';
+}, 17000)
