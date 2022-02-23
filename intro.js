1590cc58aacb35c16224c4da3a4aba071636828f
wendelaQuist <wendela140200@gmail.com>
2022-02-23 12:13:09 +0100

    last shots + camera flashes

--- intro.js
+++ intro.js
@@ -1,65 +1,84 @@
 
-
 setTimeout(function(){
     document.getElementById('second-shot').style.display = 'none';
-}, 12000)
+}, 7000)
 
 setTimeout(function(){
     document.getElementById('third-shot').style.display = 'flex';
-}, 12000)
+}, 7000)
 
 setTimeout(function(){
     document.getElementById('third-shot').style.display = 'none';
-}, 14000)
+}, 8000)
 
 setTimeout(function(){
     document.getElementById('fourth-shot').style.display = 'flex';
-}, 14000)
+}, 8000)
 
 setTimeout(function(){
     document.getElementById('fourth-shot').style.display = 'none';
-}, 18000)
+}, 12000)
 
 setTimeout(function(){
     document.getElementById('fifth-shot').style.display = 'flex';
-}, 18000)
+}, 12000)
 
 setTimeout(function(){
     document.getElementById('fifth-shot').style.display = 'none';
-}, 29000)
+}, 24000)
 
 setTimeout(function(){
     document.getElementById('sixth-shot').style.display = 'flex';
-}, 29000)
+}, 24000)
 
 setTimeout(function(){
     document.getElementById('sixth-shot').style.display = 'none';
-}, 32000)
+}, 25500)
 
 setTimeout(function(){
     document.getElementById('seventh-shot').style.display = 'flex';
-}, 32000)
+}, 25500)
 
 setTimeout(function(){
     document.getElementById('seventh-shot').style.display = 'none';
-}, 34000)
+}, 27000)
 
 setTimeout(function(){
     document.getElementById('eight-shot').style.display = 'flex';
-}, 34000)
+}, 27000)
 
 setTimeout(function(){
     document.getElementById('eight-shot').style.display = 'none';
-}, 36000)
+}, 30000)
 
 setTimeout(function(){
     document.getElementById('ninth-shot').style.display = 'flex';
-}, 36000)
+}, 30000)
 
 setTimeout(function(){
     document.getElementById('ninth-shot').style.display = 'none';
-}, 39000)
+}, 32000)
 
 setTimeout(function(){
     document.getElementById('tenth-shot').style.display = 'flex';
-}, 39000)
+}, 32000)
+
+setTimeout(function(){
+    document.getElementById('tenth-shot').style.display = 'none';
+}, 36000)
+
+setTimeout(function(){
+    document.getElementById('eleventh-shot').style.display = 'flex';
+}, 36000)
+
+setTimeout(function(){
+    document.getElementById('eleventh-shot').style.display = 'none';
+}, 42000)
+
+setTimeout(function(){
+    document.getElementById('twelfth-shot').style.display = 'flex';
+}, 42000)
+
+var audio = new Audio();
+audio.src = "music.mp3";
+audio.play();
