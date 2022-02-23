f45f8edd3f4a04914f0619dbcd2e741fcc2b9d00
wendelaQuist <wendela140200@gmail.com>
2022-02-24 00:24:20 +0100

    finished intro page

--- intro.js
+++ intro.js
@@ -1,84 +1,98 @@
+    
+    setTimeout(function(){
+        var audio = new Audio();
+        audio.src = "music.mp3";
+        audio.play();
+    }, 0)
 
-setTimeout(function(){
-    document.getElementById('second-shot').style.display = 'none';
-}, 7000)
+    setTimeout(function(){
+        document.getElementById('first-shot').style.display = 'flex';
+    }, 0)
 
-setTimeout(function(){
-    document.getElementById('third-shot').style.display = 'flex';
-}, 7000)
+    setTimeout(function(){
+        document.getElementById('second-shot').style.display = 'flex';
+    }, 0)
 
-setTimeout(function(){
-    document.getElementById('third-shot').style.display = 'none';
-}, 8000)
+    setTimeout(function(){
+        document.getElementById('second-shot').style.display = 'none';
+    }, 7000)
+    
+    setTimeout(function(){
+        document.getElementById('third-shot').style.display = 'flex';
+    }, 7000)
+    
+    setTimeout(function(){
+        document.getElementById('third-shot').style.display = 'none';
+    }, 8000)
+    
+    setTimeout(function(){
+        document.getElementById('fourth-shot').style.display = 'flex';
+    }, 8000)
+    
+    setTimeout(function(){
+        document.getElementById('fourth-shot').style.display = 'none';
+    }, 12000)
+    
+    setTimeout(function(){
+        document.getElementById('fifth-shot').style.display = 'flex';
+    }, 12000)
+    
+    setTimeout(function(){
+        document.getElementById('fifth-shot').style.display = 'none';
+    }, 24000)
+    
+    setTimeout(function(){
+        document.getElementById('sixth-shot').style.display = 'flex';
+    }, 24000)
+    
+    setTimeout(function(){
+        document.getElementById('sixth-shot').style.display = 'none';
+    }, 25500)
+    
+    setTimeout(function(){
+        document.getElementById('seventh-shot').style.display = 'flex';
+    }, 25500)
+    
+    setTimeout(function(){
+        document.getElementById('seventh-shot').style.display = 'none';
+    }, 27000)
+    
+    setTimeout(function(){
+        document.getElementById('eight-shot').style.display = 'flex';
+    }, 27000)
+    
+    setTimeout(function(){
+        document.getElementById('eight-shot').style.display = 'none';
+    }, 30000)
+    
+    setTimeout(function(){
+        document.getElementById('ninth-shot').style.display = 'flex';
+    }, 30000)
+    
+    setTimeout(function(){
+        document.getElementById('ninth-shot').style.display = 'none';
+    }, 32000)
+    
+    setTimeout(function(){
+        document.getElementById('tenth-shot').style.display = 'flex';
+    }, 32000)
+    
+    setTimeout(function(){
+        document.getElementById('tenth-shot').style.display = 'none';
+    }, 36000)
+    
+    setTimeout(function(){
+        document.getElementById('eleventh-shot').style.display = 'flex';
+    }, 36000)
+    
+    setTimeout(function(){
+        document.getElementById('eleventh-shot').style.display = 'none';
+    }, 42000)
+    
+    setTimeout(function(){
+        document.getElementById('twelfth-shot').style.display = 'flex';
+    }, 42000)
+    
+    
 
-setTimeout(function(){
-    document.getElementById('fourth-shot').style.display = 'flex';
-}, 8000)
 
-setTimeout(function(){
-    document.getElementById('fourth-shot').style.display = 'none';
-}, 12000)
-
-setTimeout(function(){
-    document.getElementById('fifth-shot').style.display = 'flex';
-}, 12000)
-
-setTimeout(function(){
-    document.getElementById('fifth-shot').style.display = 'none';
-}, 24000)
-
-setTimeout(function(){
-    document.getElementById('sixth-shot').style.display = 'flex';
-}, 24000)
-
-setTimeout(function(){
-    document.getElementById('sixth-shot').style.display = 'none';
-}, 25500)
-
-setTimeout(function(){
-    document.getElementById('seventh-shot').style.display = 'flex';
-}, 25500)
-
-setTimeout(function(){
-    document.getElementById('seventh-shot').style.display = 'none';
-}, 27000)
-
-setTimeout(function(){
-    document.getElementById('eight-shot').style.display = 'flex';
-}, 27000)
-
-setTimeout(function(){
-    document.getElementById('eight-shot').style.display = 'none';
-}, 30000)
-
-setTimeout(function(){
-    document.getElementById('ninth-shot').style.display = 'flex';
-}, 30000)
-
-setTimeout(function(){
-    document.getElementById('ninth-shot').style.display = 'none';
-}, 32000)
-
-setTimeout(function(){
-    document.getElementById('tenth-shot').style.display = 'flex';
-}, 32000)
-
-setTimeout(function(){
-    document.getElementById('tenth-shot').style.display = 'none';
-}, 36000)
-
-setTimeout(function(){
-    document.getElementById('eleventh-shot').style.display = 'flex';
-}, 36000)
-
-setTimeout(function(){
-    document.getElementById('eleventh-shot').style.display = 'none';
-}, 42000)
-
-setTimeout(function(){
-    document.getElementById('twelfth-shot').style.display = 'flex';
-}, 42000)
-
-var audio = new Audio();
-audio.src = "music.mp3";
-audio.play();
